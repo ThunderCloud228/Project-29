33c25a3b74e804eb10e3132dc03af471096779b0
ThunderCloud228 <83110279+ThunderCloud228@users.noreply.github.com>
2021-05-30 00:24:16 +0530

    Add files via upload

---
+++ js/polygon.js
@@ -0,0 +1,29 @@
+class Polygon {
+	constructor(x,y,r)
+	{
+		var options= {
+			restitution: 0,
+            friction: 1,
+			}
+		this.x = x;
+		this.y = y;
+		this.r = r;
+		this.image = loadImage("polygon.png");
+		this.body = Bodies.circle(50, 200, 20, options);
+		World.add(world, this.body);
+	}
+
+	display()
+	{
+		var polygonPos=this.body.position;	
+		push()
+		translate(polygonPos.x, polygonPos.y);
+		//rectMode(CENTER);
+		rotate(this.body.angle)
+		//fill(255,0,255)
+		imageMode(CENTER);
+		ellipseMode(CENTER);
+		image(this.image, 0,0,this.r*2, this.r*2);
+		pop()
+ }
+}
---
+++ js/slingShot.js
@@ -0,0 +1,31 @@
+class SlingShot {
+    constructor(bodyA, pointB) {
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.05,
+            length: 10
+        }
+        this.pointB = pointB;
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly() {
+        this.sling.bodyA = null;
+    }
+
+    /*attach(body) {
+        this.sling.bodyA = body;
+    }*/
+    
+    display() {
+        if(this.sling.bodyA) {
+            var bodyA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            stroke("red");
+            strokeWeight(5);
+            line(bodyA.x, bodyA.y, pointB.x, pointB.y);
+        }
+    }    
+}
